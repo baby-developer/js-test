aefaa1851aefae8113696541e66bc715fba44cf6
yeon <choiseyeoni@gmail.com>
2021-09-11 18:53:20 +0900

    Check data type

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('hihi');
+import getType from './getType'
+
+console.log(typeof 'Hello world!') // string
+console.log(typeof 123) // number
+console.log(typeof true) // boolean
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+console.log(typeof {}) // object
+console.log(typeof []) // object
+
+console.log(getType(123)) // Number
+console.log(getType(false)) // Boolean
+console.log(getType(null)) // Null
+console.log(getType({})) // Object
+console.log(getType([])) // Array
